efedd47248da1b4029829e723e955721b7457140
clarabecker <Claraeu15@outlook.com>
2024-07-09 19:39:26 -0300

    feat : Implementing db connection with repository

--- src/main/routes/trips_routes.py
+++
@@ -1,8 +0,0 @@
-from flask import jsonify, Blueprint
-
-trips_routes_bp = Blueprint("trip_routes", __name__)
-
-@trips_routes_bp.route("/trips", methods=["POST"])
-
-def createTrip():
-    return jsonify({"ola" : "mundo"}), 200
