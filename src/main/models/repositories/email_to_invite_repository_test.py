0400f5b4bfdbc98af1d8983d580e566fc5078c60
clarabecker <Claraeu15@outlook.com>
2024-07-09 20:21:26 -0300

    feat : Implementing emails repository

---
+++ src/main/models/repositories/email_to_invite_repository_test.py
@@ -0,0 +1,30 @@
+import pytest
+import uuid
+from .email_to_invite_repository import EmailsToInviteRepository
+from src.main.models.settings.db_connection_handler import db_connection_handler
+
+db_connection_handler.connect()
+trip_id = str(uuid.uuid4())
+
+@pytest.mark.skip(reason="interacao com o banco")
+def test_registryEmail():
+    conn = db_connection_handler.get_connection()
+    emails_to_invite_repository = EmailsToInviteRepository(conn)
+
+    email_trip_infos = {
+        "id" : str(uuid.uuid4()),
+        "trip_id" : trip_id,
+        "email" : "olaMundo@email.com"
+    }
+
+    emails_to_invite_repository.registryEmail(email_trip_infos)
+    
+@pytest.mark.skip(reason="interacao com o banco")
+def test_findEmailsFromTrip():
+    conn = db_connection_handler.get_connection()
+    emails_to_invite_repository = EmailsToInviteRepository(conn)
+
+    emails = emails_to_invite_repository.findEmailsFromTrip(trip_id)
+    print()
+    print(emails)
+
